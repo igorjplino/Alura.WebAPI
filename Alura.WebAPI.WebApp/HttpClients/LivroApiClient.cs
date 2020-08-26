7def3b9981231ecbfda6d00aa84402953dd8b64c
Igor Jorge <igorjplino@gmail.com>
2020-08-25 21:01:04 -0300

    Iniciando curso WebApi pt 2

---
+++ Alura.WebAPI.WebApp/HttpClients/LivroApiClient.cs
@@ -0,0 +1,37 @@
+﻿using Alura.ListaLeitura.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Alura.WebAPI.WebApp.HttpClients
+{
+    public class LivroApiClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public LivroApiClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<byte[]> GetCapaLivroAsync(int id)
+        {
+            var resposta = await _httpClient.GetAsync($"livros/{id}/capa");
+
+            resposta.EnsureSuccessStatusCode();
+
+            return await resposta.Content.ReadAsByteArrayAsync();
+        }
+
+        public async Task<LivroApi> GetLivroAsync(int id)
+        {
+            var resposta = await _httpClient.GetAsync($"livros/{id}");
+
+            resposta.EnsureSuccessStatusCode();
+
+            return await resposta.Content.ReadAsAsync<LivroApi>();
+        }
+    }
+}
